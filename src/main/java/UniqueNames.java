970c80208b8d259c2ca2badce7a5c2e085150a4f
David Trombello <dtrombello@gmail.com>
2020-01-26 14:00:17 -0500

    update uniqueNames

--- src/main/java/UniqueNames.java
+++ src/main/java/UniqueNames.java
@@ -1,7 +1,16 @@
-public class UniqueNames {
 //    Given a list of user names KEEPING THEM IN ORDER, each username needs to be unique.  For example if you have
 //    {bob, samantha, bob, robert, sarah, sarah, bob} the names will become
 //    {bob, samantha, bob1, robert, sarah, sarah1, bob2} so that each is unique.
+
+import java.util.ArrayList;
+
+public class UniqueNames {
+
+
+    public ArrayList<String> uniqueUserNames (String [] userNames) {
+        ArrayList<String> uniqueNamesList = new ArrayList<>();
+
+    }
 
 
 }
