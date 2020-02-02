7494327d6d87967f5e93db0e565ce1852f93445b
David Trombello <dtrombello@gmail.com>
2020-02-02 14:44:20 -0500

    unique user names test added and working properly

--- test/java/UniqueNamesTest.java
+++ test/java/UniqueNamesTest.java
@@ -26,6 +26,28 @@ class UniqueNamesTest {
         expected[6]="bob2";
         String [] actual = uniqueUserNamesTest.uniqueUserNames(userNames);
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void uniqueUserNames2() {
+        String [] userNames = new String[] {"mike", "samantha", "dave", "laura", "mike", "mike", "mike", "dave", "laura", "laura", "samantha", "shiela" };
+        String [] expected = new String[12] ;
+        expected[0]="mike";
+        expected[1]="samantha";
+        expected[2]="dave";
+        expected[3]="laura";
+        expected[4]="mike1";
+        expected[5]="mike2";
+        expected[6]="mike3";
+        expected[7]="dave1";
+        expected[8]="laura1";
+        expected[9]="laura2";
+        expected[10]="samantha1";
+        expected[11]="shiela";
+
+
+        String [] actual = uniqueUserNamesTest.uniqueUserNames(userNames);
+        Assertions.assertArrayEquals(expected, actual);
         //Assertions.assertEquals(expected, actual);
     }
 }
