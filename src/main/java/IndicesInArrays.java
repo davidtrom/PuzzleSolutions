e4715750fb8e0c92b29148bf215981ddeaca181a
David Trombello <dtrombello@gmail.com>
2020-02-04 22:02:01 -0500

    working on IndicesInArays

---
+++ src/main/java/IndicesInArrays.java
@@ -0,0 +1,17 @@
+//Given two arrays, the first number of the first array says how many indices there will be:
+//example: 4, 6, 0, 3, 5.  The 4 represents the amount of indices after it and those indices are 6, 0, 3, 5.
+//In the next array you will need to sort the array and then pull out the numbers at the 0, 3, 5, and 6 index to return.
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IndicesInArrays {
+
+    public ArrayList<Integer> findAtIndex (Integer [] indices, Integer [] numbersArray){
+        ArrayList <Integer> result = new ArrayList<>();
+        Arrays.sort(numbersArray);
+
+        return
+    }
+}
