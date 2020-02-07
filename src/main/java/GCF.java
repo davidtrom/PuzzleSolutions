84030da4bcb5aa53a698afbb473b40127a57e0c4
David Trombello <dtrombello@gmail.com>
2020-02-07 11:38:32 -0500

    added GCF class

---
+++ src/main/java/GCF.java
@@ -0,0 +1,22 @@
+//Find the GCF of two numbers
+
+public class GCF {
+
+    public Integer findGCF (Integer num1, Integer num2){
+        if(checkDivisible(num1, num2) != null){
+            return checkDivisible(num1, num2);
+        }
+        
+        return 0;
+    }
+
+    public Integer checkDivisible (Integer num1, Integer num2){
+        if(num1 > num2 && num1 % num2 == 0){
+            return num2;
+        }
+        else if (num2 > num1 && num2 % num1 == 0){
+            return num1;
+        }
+        else return null;
+    }
+}
