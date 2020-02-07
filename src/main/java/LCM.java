bda786e7d308b9af70443b02e0c378b1408ea16e
David Trombello <dtrombello@gmail.com>
2020-02-06 21:11:37 -0500

    more tests added and all passing in LCM

--- src/main/java/LCM.java
+++ src/main/java/LCM.java
@@ -1,6 +1,7 @@
 //Find the least common multiple of two numbers
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class LCM {
@@ -23,24 +24,25 @@ public class LCM {
             num2Multiples.add(multiple2);
             multiple2 += num2;
         }
-
         Iterator iterator = num1Multiples.iterator();
         while(iterator.hasNext()) {
             System.out.println(iterator.next());
         }
-
-            for(int i = 0; i <= num1Multiples.size(); i++){
-
+        Iterator iterator2 = num2Multiples.iterator();
+        while(iterator2.hasNext()) {
+            System.out.println(iterator2.next());
         }
-
-        return 0;
+        num1Multiples.retainAll(num2Multiples);
+        Collections.sort(num1Multiples);
+        System.out.println(num1Multiples);
+        return num1Multiples.get(0);
     }
 
     public Integer checkDivisible (Integer num1, Integer num2){
         if(num1 > num2 && num1 % num2 == 0){
             return num1;
         }
-        else if (num2 > num2 && num2 % num1 == 0){
+        else if (num2 > num1 && num2 % num1 == 0){
             return num2;
         }
         else return null;
--- test/java/LCMTest.java
+++ test/java/LCMTest.java
@@ -29,4 +29,40 @@ class LCMTest {
         Integer actual = lcmTest.findLCM(num1, num2);
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findLCM3() {
+        Integer num1 = 6;
+        Integer num2 = 42;
+        Integer expected = 42;
+        Integer actual = lcmTest.findLCM(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findLCM4() {
+        Integer num1 = 7;
+        Integer num2 = 15;
+        Integer expected = 105;
+        Integer actual = lcmTest.findLCM(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findLCM5() {
+        Integer num1 = 12;
+        Integer num2 = 18;
+        Integer expected = 36;
+        Integer actual = lcmTest.findLCM(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findLCM6() {
+        Integer num1 = 3;
+        Integer num2 = 31;
+        Integer expected = 93;
+        Integer actual = lcmTest.findLCM(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
 }
